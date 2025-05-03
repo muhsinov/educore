24af781f98e1f76555a1f5c82b7061401f44f731
unknown <you@example.com>
2025-05-03 15:13:39 +0500

    lesson_model

--- src/lesson/models.py
+++ src/lesson/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from group.models import Group
 
-# Create your models here.
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='lessons')
+    teacher = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='lessons')
+    room = models.CharField(max_length=50)
+    file = models.FileField(upload_to='lesson_files/',blank=True, null=True)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.name}({self.group.name})"
