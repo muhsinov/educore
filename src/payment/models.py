58cc7072a2839f47697b18c57a41648834e1b01e
Abdulazizdev0 <abdulazizmuhammadyunusov9@gmail.com>
2025-05-04 18:49:34 +0500

    added payment table

--- src/payment/models.py
+++ src/payment/models.py
@@ -3,7 +3,16 @@ from course.models import StudentCourse
 
 
 class Payment(models.Model):
+    pay_type = (
+        (0, "Cash"),
+        (1, "Card"),
+        (3,"Electron pay")
+    )
     student_couse = models.ForeignKey(StudentCourse, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    type = models.Choices()
+    type = models.IntegerField(choices=pay_type,default=0)
     payment_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student_couse} - {self.amount}"
+
