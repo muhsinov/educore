b0df4f70d6e8af0605e7a616df503d7a8de3c172
Abdulazizdev0 <abdulazizmuhammadyunusov9@gmail.com>
2025-05-06 20:34:50 +0500

    testing address/attendance/course/group/

---
+++ src/group/tests/test_model.py
@@ -0,0 +1,8 @@
+import unittest
+from datetime import datetime
+from address.models import Address
+from course.models import Course
+from user.models import Teacher, User, Student
+from group.models import Group,StudentGroup
+
+
--- src/homework/tests/test_model.py
+++ src/homework/tests/test_model.py
@@ -1,62 +1,105 @@
-# import pytest
-# from datetime import datetime
-# from address.models import Address
-# from group.models import Group
-# from lesson.models import Lesson
-# from course.models import Course
-# from user.models import Teacher, User, Student
-# from homework.models import Homework, StudentHomework
+import unittest
+from django.test import TestCase
+from django.utils import timezone
+from datetime import datetime
+from address.models import Address
+from group.models import Group
+from lesson.models import Lesson
+from course.models import Course
+from user.models import Teacher, User, Student
+from homework.models import Homework, StudentHomework
 
+class TestHomeworkModels(TestCase):
+    def setUp(self):
+        # Umumiy sozlashlar
+        self.course = Course.objects.create(
+            name="programing",
+            description="ertgdsertgcff",
+            cost="56789",
+            duration=30
+        )
+        self.user = User.objects.create(
+            first_name="first_name",
+            last_name="last_name",
+            birth_date=datetime.strptime("2023-01-01", "%Y-%m-%d").date(),
+            phone='0987654321',
+            joined_at=timezone.datetime(2023, 1, 1, 12, 0, tzinfo=timezone.get_default_timezone())
+        )
+        self.user1 = User.objects.create(
+            first_name="name",
+            last_name="name",
+            birth_date=datetime.strptime("2023-01-01", "%Y-%m-%d").date(),
+            phone='1234567890'
+        )
+        naive_started_at = datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S")
+        naive_end_at = datetime.strptime("2023-01-01 13:00:00", "%Y-%m-%d %H:%M:%S")
+        started_at = timezone.make_aware(naive_started_at)
+        end_at = timezone.make_aware(naive_end_at)
+        self.group = Group.objects.create(
+            name="programing",
+            course=self.course,
+            room=20,
+            started_at=started_at,
+            end_at=end_at
+        )
+        self.teacher = Teacher.objects.create(
+            user=self.user,
+            salary="56789"
+        )
+        self.student = Student.objects.create(
+            user=self.user1
+        )
+        naive_start_time = datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S")
+        naive_end_time = datetime.strptime("2023-01-01 13:00:00", "%Y-%m-%d %H:%M:%S")
+        start_time = timezone.make_aware(naive_start_time)
+        end_time = timezone.make_aware(naive_end_time)
+        self.lesson = Lesson.objects.create(
+            name="programing",
+            description="ertgdsertgcff",
+            group=self.group,
+            teacher=self.teacher,
+            room=20,
+            file="file.pdf",
+            start_time=start_time,
+            end_time=end_time
+        )
 
-# @pytest.mark.django_db
-# def test_homework_model():
-#     course = Course.objects.create(name = "programing",description = "ertgdsertgcff",cost = "56789",duration=30)
-#     user = User.objects.create(first_name = "first_name",last_name = "last_name",birth_date = datetime.strptime("2023-01-01", "%Y-%m-%d").date(),joined_at = datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S"))
-#     group = Group.objects.create(name = "programing",course = course,room = 20,started_at = datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S"),end_at = datetime.strptime("2023-01-01 13:00:00", "%Y-%m-%d %H:%M:%S"))
-#     teacher = Teacher.objects.create(user = user,salary = "56789",)
-#     lesson = Lesson.objects.create(name = "programing",description = "ertgdsertgcff",group = group,teacher = teacher,room = 20,file = "file.pdf",start_time = datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S"),end_time = datetime.strptime("2023-01-01 13:00:00", "%Y-%m-%d %H:%M:%S"))
+    def test_homework_model(self):
+        # Homework modelini sinash
+        deadline = timezone.datetime(2023, 1, 1, 12, 0, tzinfo=timezone.get_default_timezone())
+        homework = Homework.objects.create(
+            name="homework",
+            description="ertgdsertgcff",
+            lesson=self.lesson,
+            deadline=deadline
+        )
+        self.assertEqual(homework.name, "homework")
+        self.assertEqual(homework.description, "ertgdsertgcff")
+        self.assertEqual(homework.lesson, self.lesson)
+        self.assertIsNotNone(homework.deadline)
+        self.assertIsNotNone(homework.created_at)
 
-#     homework = Homework.objects.create(
-#         name = "homework",
-#         description = "ertgdsertgcff",
-#         lesson = lesson,
-#         deadline = datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S"),
-
-#     )
-#     assert homework.name == "homework"
-#     assert homework.description == "ertgdsertgcff"
-#     assert homework.lesson == lesson
-#     assert homework.deadline is not None
-#     assert homework.created_at is not None
-
-
-
-# @pytest.mark.django_db
-# def test_model_student_homework():
-#     course = Course.objects.create(name="programing", description="ertgdsertgcff", cost="56789", duration=30)
-#     user = User.objects.create(first_name="first_name", last_name="last_name",birth_date=datetime.strptime("2023-01-01", "%Y-%m-%d").date(),phone='0987654321')
-#     user1 = User.objects.create(first_name="name", last_name="name",birth_date=datetime.strptime("2023-01-01", "%Y-%m-%d").date(),phone='1234567890')
-#     group = Group.objects.create(name="programing", course=course, room=20,started_at=datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S"),end_at=datetime.strptime("2023-01-01 13:00:00", "%Y-%m-%d %H:%M:%S"))
-#     teacher = Teacher.objects.create(user=user, salary="56789", )
-#     lesson = Lesson.objects.create(name="programing", description="ertgdsertgcff", group=group, teacher=teacher,room=20, file="file.pdf", start_time=datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S"),end_time=datetime.strptime("2023-01-01 13:00:00", "%Y-%m-%d %H:%M:%S"))
-#     homework = Homework.objects.create(name="homework",description="ertgdsertgcff",lesson=lesson,deadline=datetime.strptime("2023-01-01 12:00:00", "%Y-%m-%d %H:%M:%S"),)
-#     student = Student.objects.create(user=user1)
-
-
-#     student_homework = StudentHomework.objects.create(
-#         homework = homework,
-#         student = student,
-#         text = "ertgdsertgcff",
-#         file = "file.pdf",
-#         grade = 3,
-#         teacher_advice = "ertgdsertgcff",
-#     )
-
-#     assert student_homework.homework == homework
-#     assert student_homework.student == student
-#     assert student_homework.text == "ertgdsertgcff"
-#     assert student_homework.file == "file.pdf"
-#     assert student_homework.grade == 3
-#     assert student_homework.teacher_advice == "ertgdsertgcff"
-#     assert student_homework.sended_at is not None
-
+    def test_model_student_homework(self):
+        # StudentHomework modelini sinash
+        deadline = timezone.datetime(2023, 1, 1, 12, 0, tzinfo=timezone.get_default_timezone())
+        homework = Homework.objects.create(
+            name="homework",
+            description="ertgdsertgcff",
+            lesson=self.lesson,
+            deadline=deadline
+        )
+        student_homework = StudentHomework.objects.create(
+            homework=homework,
+            student=self.student,
+            text="ertgdsertgcff",
+            file="file.pdf",
+            grade=3,
+            teacher_advice="ertgdsertgcff"
+        )
+        self.assertEqual(student_homework.homework, homework)
+        self.assertEqual(student_homework.student, self.student)
+        self.assertEqual(student_homework.text, "ertgdsertgcff")
+        self.assertEqual(student_homework.file, "file.pdf")
+        self.assertEqual(student_homework.grade, 3)
+        self.assertEqual(student_homework.teacher_advice, "ertgdsertgcff")
+        self.assertIsNotNone(student_homework.sended_at)
