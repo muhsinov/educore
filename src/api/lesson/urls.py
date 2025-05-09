00b09eefdb18d8fd2be3d971b5caeefd1718a3d5
unknown <you@example.com>
2025-05-09 16:43:59 +0500

    in lesson api bug fixed

--- src/api/lesson/urls.py
+++ src/api/lesson/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import LessonViewSet
 
 router = DefaultRouter()
-router.register('lessons', LessonViewSet)
+router.register('', LessonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
